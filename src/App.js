2a619ff553dd46a130a003a5ccff231b6513a31e
German Hernandez <muygrafico@gmail.com>
2020-06-07 22:13:38 -0500

    playing with JS linters (current JS standard)

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Greeter from "./Greeter";
+
+const App = () => (
+  <div>
+    <h1>React Function Component</h1>
+    <Greeter />
+  </div>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, "src/main.js"),
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 19000,
+    open: true,
+    hot: true, // enable HMR
+    stats: "minimal",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
 };
